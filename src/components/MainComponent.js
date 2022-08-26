12200671a968e8181671a65594bd680bc97e61ba
haitrieu <noname.r0202@gmail.com>
2022-08-26 19:40:02 +0700

    LAB09_1

---
+++ src/components/MainComponent.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import DishDetail from "./DishDetailComponent";
+import Menu from "./MenuComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+import { Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchDishes } from "../redux/ActionCreators";
+import { Loading } from "./LoadingComponent";
+
+function Main() {
+  const dishes = useSelector((state) => state.dishes);
+  const comments = useSelector((state) => state.comments);
+  const promotions = useSelector((state) => state.promotions);
+  const leaders = useSelector((state) => state.leaders);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDishes());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route exact path="/menu" element={<Menu />} />
+        <Route
+          exact
+          path="/menu/:id"
+          element={<DishDetail dishes={dishes} comments={comments} />}
+        />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/about" element={<About leaders={leaders} />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+export default Main;
---
+++ src/redux/ActionCreators.js
@@ -0,0 +1,39 @@
+import * as ActionTypes from "./ActionTypes";
+import { DISHES } from "../shared/dishes";
+
+export const addComment = (dishId, rating, author, comment) => ({
+  type: ActionTypes.ADD_COMMENT,
+  payload: {
+    dishId: dishId,
+    rating: rating,
+    author: author,
+    comment: comment,
+  },
+});
+
+export const fetchDishes = () => (dispatch) => {
+  dispatch(dishesLoading(true));
+  setTimeout(() => {
+    dispatch(addDishes(DISHES));
+  }, 1000);
+};
+
+export const dishesLoading = () => {
+  return {
+    type: ActionTypes.DISHES_LOADING,
+  };
+};
+
+export const dishesFailed = (errmess) => {
+  return {
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess,
+  };
+};
+
+export const addDishes = (dishes) => {
+  return {
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes,
+  };
+};
