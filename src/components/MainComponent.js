5cb6f11fc3bdbf4116e3aeac558c431233f0fe1b
haitrieu <noname.r0202@gmail.com>
2022-08-26 19:44:29 +0700

    LAB10_3

---
+++ src/components/MainComponent.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import DishDetail from "./DishDetailComponent";
+import Menu from "./MenuComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+import { Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function Main() {
+  const dishes = useSelector((state) => state.dishes);
+  const comments = useSelector((state) => state.comments);
+  const leaders = useSelector((state) => state.leaders);
+
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route exact path="/menu" element={<Menu />} />
+        <Route
+          exact
+          path="/menu/:id"
+          element={<DishDetail dishes={dishes} comments={comments} />}
+        />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/about" element={<About leaders={leaders} />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+export default Main;
