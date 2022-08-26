e0e1e4b47818ea2755e05f7597b87eca80e450c1
haitrieu <noname.r0202@gmail.com>
2022-08-26 19:23:02 +0700

    LAB03_2

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+
+export default App;
