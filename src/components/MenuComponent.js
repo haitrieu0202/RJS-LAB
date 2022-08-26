d53c24c30e70fde53b3a6dd1f75c585fe5113927
haitrieu <noname.r0202@gmail.com>
2022-08-26 19:32:03 +0700

    LAB06_1

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, CardImg, CardImgOverlay,
+  CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+
+function Menu(props) {
+  const menu = props.dishes.map((dish) => {
+    return (
+      <div key={dish.id} className="col-12 col-md-5 m-1">
+        <Link to={`/menu/${dish.id}`}>
+          <Card>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </Link>
+      </div>
+    );
+  });
+  return (
+    <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Menu</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Menu</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="row">{menu}</div>
+    </div>
+  );
+};
+
+export default Menu;
