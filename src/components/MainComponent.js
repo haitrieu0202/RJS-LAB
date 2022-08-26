9f8f3d960dac2aee9f999bcb08000c1b4641037c
haitrieu <noname.r0202@gmail.com>
2022-08-26 19:41:48 +0700

    LAB10_1

---
+++ src/components/MainComponent.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import DishDetail from "./DishDetailComponent";
+import Menu from "./MenuComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import About from "./AboutComponent";
+import { Routes, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchComments, fetchDishes, fetchPromos } from "../redux/ActionCreators";
+
+function Main() {
+  const dishes = useSelector((state) => state.dishes);
+  const comments = useSelector((state) => state.comments);
+  const promotions = useSelector((state) => state.promotions);
+  const leaders = useSelector((state) => state.leaders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDishes());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchComments());
+  }, [dispatch]);
+
+  useEffect(() => {
+    dispatch(fetchPromos());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route exact path="/menu" element={<Menu />} />
+        <Route
+          exact
+          path="/menu/:id"
+          element={<DishDetail dishes={dishes} comments={comments} />}
+        />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/about" element={<About leaders={leaders} />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+export default Main;
