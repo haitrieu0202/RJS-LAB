aaa3c0476cc0a59bb96549ecd1297a658b003569
haitrieu <noname.r0202@gmail.com>
2022-08-26 19:35:46 +0700

    LAB07_2

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { useParams } from "react-router-dom";
+
+const DishDetail = ({ dishes, comments }) => {
+  const { id } = useParams();
+
+  let dish = dishes.find((dish) => dish.id === parseInt(id));
+  let cmt = comments.filter((comment) => comment.dishId === parseInt(id));
+
+  const renderDish = (val) => {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <Card>
+          <CardImg width="100%" src={"/" + val.image} value={val.name} />
+          <CardBody>
+            <CardTitle>{val.name}</CardTitle>
+            <CardText>{val.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  };
+
+  const renderComments = (val) => {
+    if (val != null) {
+      return (
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <ul className="list-unstyled">
+            {val.map((comment) => {
+              return (
+                <li key={comment.id}>
+                  <p>{comment.comment}</p>
+                  <p>
+                    -- {comment.author},{" "}
+                    {new Intl.DateTimeFormat("en-US", {
+                      year: "numeric",
+                      month: "short",
+                      day: "2-digit",
+                    }).format(new Date(Date.parse(comment.date)))}
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  };
+
+  return dish ? (
+    <div className="container">
+      <div className="row">
+        {renderDish(dish)}
+        {renderComments(cmt)}
+      </div>
+    </div>
+  ) : (
+    <div></div>
+  );
+};
+
+export default DishDetail;
